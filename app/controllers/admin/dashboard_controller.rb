aa1802386fa37ea8d0ec910672ead3e793f7a20b
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2022-01-10 05:30:29 +0000

    show # of products and categories in dasboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 
   http_basic_authenticate_with :name => ENV['admin_username'], :password => ENV['admin_password']
